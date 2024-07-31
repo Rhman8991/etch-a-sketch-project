7bbeb41f4eecfdf6e06b67d2db0fb722a76d3987
rahman <rahman17wr@gmail.com>
2024-07-31 10:49:33 +0700

    Set sketchpad size and define variable to get element from DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const padSize = 600;
+    
+    const pad = document.getElementById("pad");
+    pad.style.width = `${padSize}px`;
+    pad.style.height = `${padSize}px`;
+    pad.style.border = "1px solid black"
+
+    const gridSizeChangeBtn = document.getElementById("gridSizeChangeBtn");
+})
